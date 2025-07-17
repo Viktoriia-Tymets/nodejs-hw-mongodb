341179541ad3ef1a43e736e21dab70fe549ab14a
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-07-17 10:03:52 +0100

    +

--- src/server.js
+++ src/server.js
@@ -51,6 +51,11 @@ app.use((req, res) => {
     res.status(404).json({ status: 404, message: 'Contact not found' });
   });
 }
+const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    logger.info(`🚀 Server is running on port ${PORT}`);
+  });
+
   setupServer().catch((err) => {
     console.error('❌ Failed to start server:', err);
     process.exit(1);
