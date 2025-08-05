f16cd6f8955b4916e8ccb74151929997f0796d21
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-08-05 01:01:40 +0100

    +

--- src/models/session.js
+++ src/models/session.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
 const sessionSchema = new mongoose.Schema({
-<<<<<<< HEAD
 userId: {
 type: String,
 required: true,
@@ -21,29 +20,7 @@ required: true,
 refreshTokenValidUntil: {
 type: Date,
 required: true,
-},
-=======
-  userId: {
-    type: String,
-    required: true,
-  },
-  accessToken: {
-    type: String,
-    required: true,
-  },
-  refreshToken: {
-    type: String,
-    required: true,
-  },
-  accessTokenValidUntil: {
-    type: Date,
-    required: true,
-  },
-  refreshTokenValidUntil: {
-    type: Date,
-    required: true,
-  },
->>>>>>> 8ce58a10cc4357f0013dd66e0325e0d6b31f2d33
+}
 });
 
 export const Session = mongoose.model('Session', sessionSchema)
