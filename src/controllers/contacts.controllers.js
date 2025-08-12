d11a3b40d0393395fd303e6e42ec5d745764eb83
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-08-12 10:25:21 +0100

    done

--- src/controllers/contacts.controllers.js
+++ src/controllers/contacts.controllers.js
@@ -1,7 +1,12 @@
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+
 import { getAllContacts, getContactById, createContact, updateContact, deleteContact } from "../services/contacts.js";
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
 import createError from 'http-errors';
+import { uploadToCloudinary } from "../utils/uploadToCloudinary.js";
+import { getEnvVariable } from '../utils/getEnvVariable.js';
 
 export async function handleGetAllContacts(req, res, next) {
     try {
@@ -24,6 +29,10 @@ export async function handleGetAllContacts(req, res, next) {
 
 export async function handleGetAllContactsById(req, res, next) {
     const {contactId} = req.params;
+    const updates = req.body;
+    if (req.file) {
+      updates.photo = req.file.path;
+    }
     const contact = await getContactById(contactId, req.user._id);
 
     if (!contact) {
@@ -37,30 +46,69 @@ export async function handleGetAllContactsById(req, res, next) {
     });
 }
 
-export async function handleCreateContact(req, res) {
-    const contact = await createContact(req.body, req.user._id);
+export async function handleCreateContact(req, res, next) {
+  try {
+    let photoUrl = null;
+
+    if (req.file) {
+      if (getEnvVariable('UPLOAD_TO_CLOUDINARY') === 'true') {
+        const result = await uploadToCloudinary(req.file.path);
+        await fs.unlink(req.file.path);
+        photoUrl = result.secure_url;
+      } else {
+        await fs.rename(
+          req.file.path,
+          path.resolve('src/uploads/avatars', req.file.filename)
+        );
+        photoUrl = `http://localhost:3000/avatars/${req.file.filename}`;
+      }
+    }
+
+    const contact = await createContact({ ...req.body, photo: photoUrl }, req.user._id);
+
     res.status(201).json({
         status: 201,
         message: 'Successfully created a contact!',
         data: contact,
     })
-    
+  } catch (error) {
+    next(error);
+}
 }
 
-export async function handleUpdateContact(req, res) {
+export async function handleUpdateContact(req, res, next) {
     const { contactId } = req.params;
-    const result = await updateContact(contactId, req.body, req.user._id);
+    const updates = { ...req.body };
 
-    if(!result) {
-        throw createError(404, 'Contact not found');
+    if (req.file) {
+      if (getEnvVariable('UPLOAD_TO_CLOUDINARY') === 'true') {
+        const result = await uploadToCloudinary(req.file.path);
+        await fs.unlink(req.file.path);
+        updates.photo = result.secure_url;
+      } else {
+        await fs.rename(
+          req.file.path,
+          path.resolve('src/uploads/avatars', req.file.filename)
+        );
+        updates.photo = `http://localhost:3000/avatars/${req.file.filename}`;
+      }
+    }
+
+    const updated = await updateContact(contactId, updates, req.user._id);
+
+    if (!updated) {
+      throw createHttpError(404, 'Contact not found');
     }
 
     res.json({
         status: 200,
         message: 'Successfully patched a contact!',
-        data: result
+        data: updated
     })
+   } catch (err) {
+  next(err);
 }
+
 
 export async function handleDeleteContact(req, res) {
     const { contactId } = req.params;
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { authenticate } from '../middlewares/authenticate.js';
+import { uploadFile } from '../middlewares/uploadFile.js';
 
 import {
-    handleGetAllContacts,
-    handleGetAllContactsById,
-    handleCreateContact,
-    handleUpdateContact,
-    handleDeleteContact
-
-} from '../controllers/contacts.controllers.js'
+  handleGetAllContacts,
+  handleGetAllContactsById,
+  handleCreateContact,
+  handleUpdateContact,
+  handleDeleteContact,
+} from '../controllers/contacts.controllers.js';
 import isValidId from '../middlewares/isValidId.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { contactsSchema, updateContactSchema } from '../validation/contacts.js';
@@ -18,8 +18,19 @@ router.use(authenticate);
 
 router.get('/', ctrlWrapper(handleGetAllContacts));
 router.get('/:contactId', isValidId, ctrlWrapper(handleGetAllContactsById));
-router.post('/', validateBody(contactsSchema), ctrlWrapper(handleCreateContact));
-router.patch('/:contactId', validateBody(updateContactSchema), isValidId, ctrlWrapper(handleUpdateContact));
-router.delete('/:contactId', isValidId, ctrlWrapper(handleDeleteContact))
+router.post(
+  '/',
+  uploadFile.single('photo'),
+  validateBody(contactsSchema),
+  ctrlWrapper(handleCreateContact),
+);
+router.patch(
+  '/:contactId',
+  uploadFile.single('photo'),
+  validateBody(updateContactSchema),
+  isValidId,
+  ctrlWrapper(handleUpdateContact),
+);
+router.delete('/:contactId', isValidId, ctrlWrapper(handleDeleteContact));
 
 export default router;
