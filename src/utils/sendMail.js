1bd48f9c06906b88d559ae1077a6757a12acc57c
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-08-13 22:27:53 +0100

    done

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,20 +1,28 @@
 import nodemailer from 'nodemailer';
-
 import { getEnvVariable } from './getEnvVariable.js';
 
-const transporter = nodemailer.createTransport({
-  host: getEnvVariable('SMTP_HOST'),
-  port: Number(getEnvVariable('SMTP_PORT')),
-  secure: false,
-  auth: {
-    user: getEnvVariable('SMTP_LOGIN'),
-    pass: getEnvVariable('SMTP_PASSWORD'),
-  },
-});
+let transporter;
+
+function initTransporter() {
+  if (!transporter) {
+    transporter = nodemailer.createTransport({
+      host: getEnvVariable('SMTP_HOST'),
+      port: Number(getEnvVariable('SMTP_PORT')),
+      secure: false,
+      auth: {
+        user: getEnvVariable('SMTP_LOGIN'),
+        pass: getEnvVariable('SMTP_PASSWORD'),
+      },
+    });
+  }
+}
 
 const sendEmail = (mail) => {
-    mail.from = getEnvVariable('SMTP_FROM'); 
-    return transporter.sendMail(mail);
-  };
-  
-  export default sendEmail;
+  initTransporter();
+
+  mail.from = getEnvVariable('SMTP_FROM');
+  return transporter.sendMail(mail);
+};
+
+export default sendEmail;
+
--- src/utils/uploadToCloudinary.js
+++ src/utils/uploadToCloudinary.js
@@ -1,16 +1,20 @@
 import { v2 as cloudinary } from 'cloudinary';
 
-import { getEnvVariable } from './getEnvVariable.js';
+let isConfigured = false;
 
-cloudinary.config({
-  cloud_name: getEnvVariable('CLOUDINARY_CLOUD_NAME'),
-  api_key: getEnvVariable('CLOUDINARY_API_KEY'),
-  api_secret: getEnvVariable('CLOUDINARY_API_SECRET'),
-});
+export function configureCloudinary() {
+  if (isConfigured) return;
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+  isConfigured = true;
+}
 
 export function uploadToCloudinary(filePath) {
-    return cloudinary.v2.uploader.upload(filePath);
-  }
-  
+  configureCloudinary();
+  return cloudinary.uploader.upload(filePath);
+}
 
   
