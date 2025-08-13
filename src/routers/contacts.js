705ff36438a4519d808ed36e562f68edc6844113
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-08-13 10:57:11 +0100

    +

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import multer from 'multer';
 import { authenticate } from '../middlewares/authenticate.js';
 import { uploadFile } from '../middlewares/uploadFile.js';
 
--- src/utils/getEnvVariable.js
+++ src/utils/getEnvVariable.js
@@ -1,9 +1,10 @@
 import createHttpError from 'http-errors';
 
 export function getEnvVariable(name) {
-  const value = process.env[name];
-  if (!value) {
-    throw createHttpError(500, `Cannot read variable ${name} from process.env`);
+    const value = process.env[name];
+    console.log(`Env var ${name}:`, value);
+    if (value === undefined || value === '') {
+      throw createHttpError(500, `Cannot read variable ${name} from process.env`);
+    }
+    return value;
   }
-  return value;
-}
