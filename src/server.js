27cce7fd26fcb530dc213fae195c3665333a6c23
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-08-18 11:13:13 +0100

    +

--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
-import swaggerUi from 'swagger-ui-express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import YAML from 'yamljs';
 import express from 'express';
 import cors from 'cors'
 import pino from 'pino'
 import pinoHttp from 'pino-http'
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import contactsRouter from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
@@ -22,9 +19,7 @@ async function setupServer() {
 
 const app = express();
 const logger = pino();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const swaggerDocument = YAML.load(path.join(__dirname, '../docs/openapi.yaml'));
+
 
 
 await initMongoConnection();
@@ -34,10 +29,9 @@ app.use(pinoHttp({ logger }))
   app.use(express.json())
   app.use(cookieParser());
   app.use('/avatars', express.static('src/uploads/avatars'));
-
   app.use('/contacts', contactsRouter);
   app.use('/auth', userRouter);
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use('/api-docs', swaggerDocs());
   app.use(notFoundHandler);
   app.use(errorHandler);
   
